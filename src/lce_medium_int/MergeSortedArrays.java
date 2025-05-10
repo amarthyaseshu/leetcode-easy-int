c61ca8a468a36221fa2fbcf69fe27bdb1b44ccd3
amarthya <amarthyaseshu2@gmail.com>
2025-05-10 23:45:38 +0530

    commit

--- src/lce_medium_int/MergeSortedArrays.java
+++ src/lce_medium_int/MergeSortedArrays.java
@@ -19,16 +19,25 @@ public class MergeSortedArrays {
         int n=3;
         int[] nums1={2,3,6,0,0,0};
         int[] nums2={1,2,4};
-        int currentHighestIndex=nums1.length-1;
-        while (n>0) {
-                if(nums2[n-1]>nums1[m-1]){
-                    nums1[currentHighestIndex]=nums2[n-1];
-                    n--;
-                }else{
-                    nums1[currentHighestIndex]=nums1[m-1];
-                    m--;
-                }
-            currentHighestIndex--;
+        int p=(m+n)-1;
+        int p1=m-1;
+        int p2=n-1;
+        while(p1>=0 && p2>=0){
+            if(nums2[p2]>=nums1[p1]){
+                nums1[p]=nums2[p2];
+                p2--;
+                p--;
+            }else{
+                nums1[p]=nums1[p1];
+                p1--;
+                p--;
+            }
+
+        }
+        while(p2>=0){
+            nums1[p]=nums2[p2];
+            p2--;
+            p--;
         }
         System.out.println(Arrays.toString(nums1));
     }
